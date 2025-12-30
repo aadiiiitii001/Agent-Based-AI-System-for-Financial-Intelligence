8d26cf515f5219047c67fca9837888169f77d51b
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-12-30 14:13:07 +0530

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./finance_ai.db"  # replace with PostgreSQL later
+DATABASE_URL = "postgresql://task_management_db_jbl0_user:udergQYDYSwFaHHIA1gXYbmKZPWywuwA@dpg-d53vh08gjchc73fh2to0-a/task_management_db_jbl0"  # replace with PostgreSQL later
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
